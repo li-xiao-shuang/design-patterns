c7ebe907367dbeea47f6465438a72c83f4a22a81
lixiaoshuang <644968328@qq.com>
2022-02-14 20:33:26 +0800

    原型模式

---
+++ design-prototype-01/src/test/java/QuestionBankControllerTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author lixiaoshuang
+ */
+public class QuestionBankControllerTest {
+    
+    @Test
+    public void createPaper() {
+        QuestionBankController questionBankController = new QuestionBankController();
+        System.out.println(questionBankController.createPaper("花花", "1000001921032"));
+        System.out.println(questionBankController.createPaper("豆豆", "1000001921051"));
+        System.out.println(questionBankController.createPaper("大宝", "1000001921987"));
+    }
+}
