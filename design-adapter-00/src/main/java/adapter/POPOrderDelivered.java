4ca6d7a69c685ecd2447a3e98834e7666f7920e3
lixiaoshuang <644968328@qq.com>
2022-02-21 22:37:28 +0800

    适配器例子

---
+++ design-adapter-00/src/main/java/adapter/POPOrderDelivered.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2021 Gypsophila open source organization.
+ *
+ * Licensed under the Apache License,Version2.0(the"License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package adapter;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ * @author lixiaoshuang
+ */
+@Data
+public class POPOrderDelivered {
+
+    private String uId;     // 用户ID
+    private String orderId; // 订单号
+    private Date orderTime; // 下单时间
+    private Date sku;       // 商品
+    private Date skuName;   // 商品名称
+
+    private BigDecimal decimal; // 金额
+}
---
+++ design-adapter-00/src/main/java/service/POPOrderService.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2021 Gypsophila open source organization.
+ *
+ * Licensed under the Apache License,Version2.0(the"License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author lixiaoshuang
+ */
+public class POPOrderService {
+
+    private Logger logger = LoggerFactory.getLogger(POPOrderService.class);
+
+    public boolean isFirstOrder(String uId) {
+        logger.info("POP商家，查询用户的订单是否为首单：{}", uId);
+        return true;
+    }
+}
